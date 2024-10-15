997600afc0d023e8ff72475a258047769eb08917
Phoebe Fletcher <Phoebe.Fletcher@flutteruki.com>
2024-10-15 09:19:19 +0100

    Added connect4 css

--- src/components/ConnectFour/Grid.js
+++ src/components/ConnectFour/Grid.js
@@ -1,22 +1,28 @@
 import React from "react";
 import GridImage from "./ConnectFourGrid.png";
+import FrontJaffa from "./FrontJaffaCake.png";
+import './Connect4.css'; // Import the CSS file
 
 const Grid = () => {
     return (
-        <div
-            style={{
-                background: "#87CEFA",
-                marginTop: "0px",
-                height: "500px",
-                padding: "20px",
-                borderRadius: "10px",
-                boxSizing: "border-box",
-            }}
-        >
-            <img src={GridImage} alt="Connect Four Grid" />
+        <div className="grid-container">
+            {/* Grid image */}
+            <img
+                src={GridImage}
+                alt="Connect Four Grid"
+                className="grid-image"
+            />
+
+            {/* Jaffa Token image */}
+            <img
+                src={FrontJaffa}
+                alt="Front JaffaCake"
+                className="jaffa-token"
+            />
         </div>
     );
 };
 
 export default Grid;
+
 
