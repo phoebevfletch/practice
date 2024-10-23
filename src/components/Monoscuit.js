c8baf2dd1344a9b3d4bf64bf521f42a780b337c7
Phoebe Fletcher <Phoebe.Fletcher@flutteruki.com>
2024-10-23 10:54:41 +0100

    monoscuit conspiracy theory

---
+++ src/components/Monoscuit.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Monoscuit = () => (
+    <div>
+        <h1>The Death of the Biscuit: Why Biscuits don't exist anymore </h1>
+
+        <p>The word "biscuit" has been hijacked, twisted, and turned into a lie.
+            The biscuits you see in supermarkets aren't what you think they are.
+            They are a government conspiracy. Originally, biscuits
+            were a durable, non-perishable food created for soldiers and sailors, derived
+            from the Latin <i>bis coctus</i>, meaning "twice cooked." These were the <i>real</i> biscuits,
+            designed to last through the harshest conditions. But the biscuits you see today? They're Imposters,
+            nothing more than <i>monoscuits</i>.</p>
+
+        <p>The government, in collaboration with big food corporations have brainwashed us into thinking these
+            single-baked soggy imposters are the same. Why? Because it's cheaper, easier and faster
+            to bake them only once. Efficiency over quality- profit over truth. They have cut the
+            second bake and stripped biscuits of their identity, and we have been none the wiser.
+        </p>
+        <p>But here's the kicker, the modern "biscuit" is designed to go stale faster on purpose. soft,
+            perishable and unreliable-this is all part of a larger scheme. By making them degrade quickly,
+            you consume them quicker and we always go back for more.
+        </p>
+        <p>But here is where the conspiracy deepens - Jaffa Cakes. The infamous debate:
+            is it a cake or a biscuit? The truth is, Jaffa Cakes are cakes, legally and literally. They
+            always were, but that lawsuit was McVitie's greatest marketing ploy. It wasn't about biscuits or
+            cakes, it was about generating attention. And it worked - JaffaCakes became their biggest success
+        </p>
+        <p>While we sit around arguing about cakes and biscuits, the real question—the one they don't want us to ask—is
+            far more dangerous: What happend to that second-bake?
+        </p>
+        <p>It's all part of a carefully orchestrated marketing ploy, a smokescreen designed to hide the fact
+            that true biscuits no longer exist. The biscuit has become a victim of modern convenience, stripped of its historical roots and paraded
+            under a comforting, but ultimately deceptive, name. Biscuits, as we know them today, are nothing more than
+            monoscuits in disguise—just another piece in a grand government-endorsed marketing scheme to keep us compliant,
+            distracted, and in the dark.</p>
+    </div>
+);
+
+export default Monoscuit;
